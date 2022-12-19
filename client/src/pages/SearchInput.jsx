1510aada69f2f80aafb709ada36c3fb4c8341c9f
Yevgeniy Sudakov <yevgeniysudakov@gmail.com>
2022-12-19 15:44:23 +0300

    style: add styles for search block in tours page

--- client/src/pages/SearchInput.jsx
+++ client/src/pages/SearchInput.jsx
@@ -1,39 +1,27 @@
-import React, { useState } from 'react';
+import React from 'react';
 import TourStore from '../store/TourStore';
 
 const SearchInput = ({ searchHandler }) => {
-    const [action, isAction] = useState(true);
-
     return (
-        <div className='flex w-full justify-center'>
-            <label htmlFor='table-search' className='sr-only'>
-                Search
-            </label>
-            <div className='relative'>
-                <div className='flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none'>
-                    <svg
-                        className='w-5 h-5 text-gray-500 dark:text-gray-400'
-                        aria-hidden='true'
-                        fill='currentColor'
-                        viewBox='0 0 20 20'
-                        xmlns='http://www.w3.org/2000/svg'
-                    >
-                        <path
-                            fillRule='evenodd'
-                            d='M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z'
-                            clipRule='evenodd'
-                        ></path>
-                    </svg>
+        <div className='flex w-full justify-center '>
+            <div className='bg-black w-1/2  h-[150px] items-center  justify-items-center'>
+                <label htmlFor='table-search' className='sr-only'>
+                    Search
+                </label>
+                <div className='relative flex justify-around items-center  justify-items-center h-full'>
+                    <div className='md:visible invisble md:text-[30px] md:font-nano md:text-gray-200'>
+                        {' '}
+                        Find tour:{' '}
+                    </div>
+                    <input
+                        type='text'
+                        id='table-search-users'
+                        className='text-bold block p-5 pl-10 w-80 text-sm  rounded-lg border  bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500'
+                        placeholder='Find tour'
+                        onChange={searchHandler}
+                        defaultValue={TourStore.searchInput}
+                    />
                 </div>
-                <div className='text-[30px]'> Find tour: </div>
-                <input
-                    type='text'
-                    id='table-search-users'
-                    className='text-bold block p-5 pl-10 w-80 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
-                    placeholder='Find tour'
-                    onChange={searchHandler}
-                    defaultValue={TourStore.searchInput}
-                />
             </div>
         </div>
     );
